9c18569f9af0bcb7ea6bf59358e496cea42dcab3
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-03 01:00:11 +0200

    simplify terminator-track-gen.bash, add "-a" option to include --all-context predictions

--- terminator-track-gen.bash
+++ terminator-track-gen.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+all=
+if [[ "$1" = -a* ]]
+then
+	shift
+	all=\ --all-context
+fi
+
 gb=$1
 
 id=${gb##*/} id=${id%.gb}
@@ -18,7 +25,7 @@ set -C # If set, bash does not overwrite an existing
 gb2ptt.py $gb > $ptt
 gb2fasta.py $gb > $fa
 sed -i "1s/.*/>$id/" $fa
-transterm.bash --all-context $fa $ptt > $t
+transterm.bash$all $fa $ptt > $t
 
         # goal:
         #       track name="BED" description="BED Test Track" visibility=2 itemRgb="on" 
@@ -27,17 +34,17 @@ transterm.bash --all-context $fa $ptt > $t
         #       chr     400     425     Baz     800     -       405     420     0,0,255
 
 {
-	echo 'track name="BED" description="BED Test Track" visibility=2 itemRgb="on"'
+	echo 'track name="TransTermHP'"$all"'" description="TransTermHP Terminator Predictions'"$all"'" visibility=2 itemRgb="on"'
 
 	< $t grep "^  TERM" |
-		while read line
+		while read term id beg dash end str pos con rest
 	do
-		id=${line:5:6} str=${line:31:1} con=${line:37:3} col=$((con*128))
-		if [ "$str" = "+" ]
-		then beg=${line:12:7} end=${line:22:7}
-		else beg=${line:22:7} end=${line:12:7}
+		col=$((100-con))	# let the color vary between 0 (black) and 100 (gray)
+		if [ "$str" = "-" ]	# switch begin & end if on reverse strand
+		then tmp=$beg beg=$end end=$tmp
 		fi
 		echo "chr	$beg	$end	TERM_$id	$con	$str	$beg	$end	$col,$col,$col"
 	done
-} \
-	> $id-transterm-all.bed
+}
+
+echo "temporary files stored in '$tmpdir'" >&2
