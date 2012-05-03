878155c5769683702332e477ebe4ffa0b51ede2c
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-03 18:18:40 +0200

    move stuff into "source" directory & a bunch of other reorganizations

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python
 
-from distutils.core import Command, setup
-from unittest import TextTestRunner, defaultTestLoader
-
-# custom setup commands:
-#  https://da44en.wordpress.com/2002/11/22/using-distutils/
-
-class TestCommand(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        TextTestRunner().run(
-			defaultTestLoader.discover("test", "*"))
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup
 
 setup(
 	name='RNA-seqlyze',
 	version='0.1',
-	cmdclass={
-		'test': TestCommand
-	})
+	test_suite='test')
