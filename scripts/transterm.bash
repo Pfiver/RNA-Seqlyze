7c6d3c570d3df546892bf4cccba16e6fba124695
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-08 11:51:18 +0200

    save changes on soon to be replaced bash & "make" scripts

--- scripts/transterm.bash
+++ scripts/transterm.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SRC=$(dirname "$(readlink -f "$0")")
-
-SRC=$(readlink -f "$SRC/../TransTermHP/transterm_hp_v2.06")
+SRC=$(readlink -f "$SRC/../../TransTermHP/transterm_hp_v2.06")
 
 $SRC/transterm -p $SRC/expterm.dat "$@"
