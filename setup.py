9be8c6cb275c8b081ddc24b0918d0b16860f16f8
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-08 11:37:28 +0200

    remove distribute_setup.py & refer to it in setup.py instead

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
+# encoding: utf-8
 
-import distribute_setup
-distribute_setup.use_setuptools()
+# this "distrubute" setup script depends on
+
+	# "distribute" 0.6.26, by Tarek Ziadé
+	# - source code: https://bitbucket.org/tarek/distribute
+	# - documentation: http://pypi.python.org/pypi/distribute
+
+	# to run this script, "distribute" must be installed, as follows:
+	# 1) fetch the setup script
+	#     $ curl -O https://bitbucket.org/tarek/distribute/raw/b69f072c0002/distribute_setup.py
+	# 2) install "distribute"
+	#     $ python distribute_setup.py --user
+
 from setuptools import setup
 
 setup(
 	name='RNA-seqlyze',
 	version='0.1',
-	test_suite='test')
+	test_suite='test'
+)
