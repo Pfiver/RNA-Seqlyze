4c2fac6ee1557bbc05eadf41df37cc4a6cd3628b
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-20 08:21:19 +0200

    give setup.py an overhaul, based on the bcbb one

--- src/rna-seqlyze/setup.py
+++ src/rna-seqlyze/setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # encoding: utf-8
 
 # this "distrubute" setup script depends on
@@ -14,9 +14,28 @@
 	#     $ python distribute_setup.py --user
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
 	name='RNA-seqlyze',
 	version='0.1',
-	test_suite='test'
+	test_suite='test',
+	license = "Mixed",
+	author_email = "patrick@patrickpfeifer.net"
+	description = "RNA-seq analysis & sequence annotation enhancement web-application",
+	license = "MIT",
+	url = "http://git.pfeifer.ch/rna-seqlyze.git",
+	packages = find_packages(),
+    scripts = [
+		'scripts/xml_tool',
+	],
+	install_requires = [
+#		"Cython >= 0.16",
+		"pysam >= 0.6",
+		"Trac >= 0.13dev",
+		"MySQL-python 1.2.2",	# for Trac wiki
+		"MarkupSafe >= 0.15",	# required by Trac
+		"biopython >= 1.59",
+		"bcbio_nextgen >= 0.3a",
+	],
 )
