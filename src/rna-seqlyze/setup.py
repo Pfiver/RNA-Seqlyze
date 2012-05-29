177273850c19c6c24b53997f85568aeb8e3a7252
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-05-29 11:49:52 +0200

    comment out "MySQL-python >= 1.2.2" install_requires to make buildbot happy

--- src/rna-seqlyze/setup.py
+++ src/rna-seqlyze/setup.py
@@ -13,6 +13,8 @@
     # 2) install "distribute"
     #     $ python distribute_setup.py --user
 
+import os
+
 from setuptools import setup
 from setuptools import find_packages
 
@@ -26,19 +28,17 @@ setup(
     license="Mixed",
     packages=find_packages(),
     test_suite='nose.collector',
-    scripts=[
-        'scripts/xml_tool',
-        'scripts/trac-mysqlbackup',
-    ],
+    scripts=['scripts/' + name
+        for name in os.listdir("scripts")],
     setup_requires=[
-
+        "nose >= 1.0",
     ],
     install_requires=[
         "nose >= 1.0",
 #       "Trac >= 0.13dev",
         "pyramid >= 1.3.2",
         "MarkupSafe >= 0.15",       # required by Trac
-        "MySQL-python >= 1.2.2",    # for the Trac wiki
+#       "MySQL-python >= 1.2.2",    # for the Trac wiki
         "distribute >= 0.6.14",     # unsure if really required, but won't harm
         "python-daemon >= 1.6",
     ],
