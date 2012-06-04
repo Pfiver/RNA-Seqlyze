ff1c9ed350cfee6cb9982778c1418cb94fa5b83f
Twitter, Inc <opensource@twitter.com>
2012-06-05 00:14:54 +0200

    physically copy all assets to bootstrap.web:img,css,js

--- src/rna-seqlyze-web/rnaseqlyze/web/__init__.py
+++ src/rna-seqlyze-web/rnaseqlyze/web/__init__.py
@@ -9,8 +9,9 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('base', '/')
+    for path in 'img', 'css', 'js':
+        config.add_static_view(path, path)
     config.scan()
     return config.make_wsgi_app()
 
