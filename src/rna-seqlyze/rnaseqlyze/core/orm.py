8b3d8157a03e1d4099a0814ca8023f53144ebe9d
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-06-07 15:38:48 +0200

    initial version of core/orm.py

---
+++ src/rna-seqlyze/rnaseqlyze/core/orm.py
@@ -0,0 +1,75 @@
+"""
+Map Python Objects to Database Tables
+"""
+
+# infrastructure
+################
+
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declared_attr, declarative_base
+
+class Entity(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+
+Entity = declarative_base(cls=Entity)
+
+
+# entities
+##########
+
+class Analysis(Entity):
+
+    id = Column(Integer, primary_key=True)
+    refseq_ns = Column(String) # RefSeq accession
+                               # maps directly to filename
+
+    # rnaseq_study = `backref` from RNASeqStudy
+    # feature_predictions = `backref` from FeaturePredictions
+    # hg_tracks = `backref` from HgTrack
+
+# SRA analogons
+
+class RNASeqStudy(Entity):
+    """
+    Constitues an SRA "SRP" == SRA Study
+
+    Holds the "SRP...." identifier
+    """
+    srp = Column(String, primary_key=True)
+    # experiments = `backref` from RNASeqExperiment
+
+class RNASeqExperiment(Entity):
+    """
+    Constitutes an SRA "SRX" == SRA Experiment
+    """
+    srx = Column(String, primary_key=True)
+    srp = relationship("RNASeqStudy", backref=backref("experiments"))
+    # runs = `backref` from RNASeqRun
+
+class RNASeqRun(Entity):
+    """
+    Constitutes an SRA "SRR" == SRA Run
+    """
+    srr = Column(String, primary_key=True)
+    srx = relationship("RNASeqExperiment", backref=backref("runs"))
+
+# predictions
+
+class FeaturePredictions(Entity):
+    """
+    Holds a reference to the output of a FeaturePredictor
+    """
+    type = Column('type', String(50))
+
+# tracks
+
+class HgTrack(Entity):
+    """
+    Holds the type and filename for a UCSC Genome Browser Track
+    """
+    type = Column('type', String(50))
