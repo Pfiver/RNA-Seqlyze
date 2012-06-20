c6be6d8d28e55ff3045e281895b7e05143af66cc
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-06-20 18:17:52 +0200

    version 2 of apidoc.py

--- src/rna-seqlyze-cli/rnaseqlyze/cli/apidoc.py
+++ src/rna-seqlyze-cli/rnaseqlyze/cli/apidoc.py
@@ -1,54 +1,40 @@
 import sys
-from os import path
 from string import Template
 from pkgutil import walk_packages
-from collections import defaultdict
 
-Node = lambda: NodeType(Node)
-NodeType = type('Node', (defaultdict,), dict())
+Package = type('Package', (object,), dict())
 
 def main(argv=sys.argv):
-    tree = Node()
-    tree.name = 'index'
-    tree.is_pkg = True
+    packages = {}
     for loader, name, is_pkg in walk_packages(argv[1:]):
-        if name == 'setup':
-            continue
-        node = tree
-        for module in name.split('.'):
-            node = node[module]
-        node.is_pkg = is_pkg
-        node.name = name
-    walk(tree)
-
-def walk(node):
-    assert node.is_pkg == (len(node) > 0)
-    if node.is_pkg:
-        subs = []
-        print("writing", node.name + '.rst')
-        #outfile = open(node.name + '.rst', 'w')
-        outfile = sys.stdout
-        outfile.write(package.substitute(name=node.name, equals="="*len(node.name)))
-        outfile.write(module.substitute(name=node.name, dashes="-"*len(node.name)))
-        for k, v in node.items():
-            if not v.is_pkg:
-                outfile.write(module.substitute(name=v.name, dashes="-"*len(v.name)))
-            else:
-                subs.append(v.name)
-                walk(v)
-        if subs:
-            outfile.write(subpackages.substitute(names="\n\t".join(subs)))
-        #outfile.close()
+        pkgname = name.rsplit('.', 1)[0]
+        if is_pkg:
+            print name
+            if pkgname != name:
+                packages[pkgname].subpackages.append(name)
+            pkg = packages[name] = Package()
+            pkg.subpackages = []
+            pkg.outfile = open(name + '.rst', 'w')
+            pkg.outfile.write(package.substitute(name=name, equals="="*len(name)))
+        else:
+            # skip modules that are not part of any package
+            if pkgname not in packages:
+                continue
+            pkg = packages[pkgname]
+        pkg.outfile.write(module.substitute(name=name, dashes="-"*len(name)))
+    for pkg in packages.values():
+        if pkg.subpackages:
+            pkg.outfile.write(subpackages.substitute(names="\n\t".join(pkg.subpackages)))
 
 package = Template("""\
 Package `${name}`
-${equals}========
+${equals}==========
 
 """)
 
 module = Template("""\
 :mod:`${name}`
-${dashes}-----
+${dashes}-------
 
 .. automodule:: ${name}
 	:members:
@@ -62,7 +48,6 @@ Subpackages
 -----------
 
 .. toctree::
-
 	${names}
 
 """)
