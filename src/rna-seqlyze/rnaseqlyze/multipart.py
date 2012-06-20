17eb53e9159c619bc47ea726a88763c24502cca3
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-06-20 22:47:44 +0200

    multipart.py: re-indent with spaces
    added a reST formatted docstring as well

--- src/rna-seqlyze/rnaseqlyze/multipart.py
+++ src/rna-seqlyze/rnaseqlyze/multipart.py
@@ -1,41 +1,48 @@
 # based on http://code.activestate.com/recipes/146306/
 
 def urlopen(url, data):
-	import urllib2
-	req = urllib2.Request(url, data)
-	try:
-		boundary = data[2:data.index("\r")]
-	except ValueError, e:
-		raise Exception("couldn't find boundary string in data", e)
-	req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
-	return urllib2.urlopen(req)
+    import urllib2
+    req = urllib2.Request(url, data)
+    try:
+        boundary = data[2:data.index("\r")]
+    except ValueError, e:
+        raise Exception("couldn't find boundary string in data", e)
+    req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+    return urllib2.urlopen(req)
 
-def urlencode(fields, files=None):
-	"""
-	fields is a sequence of (name, value) elements for regular form fields.
-	files is a sequence of (name, filename, value) elements for data to be uploaded as files
-	Return "multipart/form-data" encoded fields + files
-	"""
-	import uuid
-	boundary = str(uuid.uuid4())
-	data = []
-	for (key, value) in fields:
-		data.append('--' + boundary)
-		data.append('Content-Disposition: form-data; name="%s"' % key)
-		data.append('')
-		data.append(value)
-	if files:
-		for (key, filename, value) in files:
-			data.append('--' + boundary)
-			data.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-			data.append('Content-Type: %s' % get_content_type(filename))
-			data.append('')
-			data.append(value)
-	data.append('--' + boundary + '--')
-	data.append('')
+def m1(asd, qwe, yxc):
+    """
+    :param asd:
+      is a sequence of ``(name, value)`` elements for regular form fields.
 
-	return '\r\n'.join(data)
+    :param files:
+      is a sequence of ``(name, filename, value)``
+      elements for data to be uploaded as files
+
+    :returns:
+      ``str`` of **multipart/form-data** encoded fields + files
+    """
+    import uuid
+    boundary = str(uuid.uuid4())
+    data = []
+    for (key, value) in fields:
+        data.append('--' + boundary)
+        data.append('Content-Disposition: form-data; name="%s"' % key)
+        data.append('')
+        data.append(value)
+    if files:
+        for (key, filename, value) in files:
+            data.append('--' + boundary)
+            data.append('Content-Disposition: form-data' \
+                            '; name="%s"; filename="%s"' % (key, filename))
+            data.append('Content-Type: %s' % get_content_type(filename))
+            data.append('')
+            data.append(value)
+    data.append('--' + boundary + '--')
+    data.append('')
+
+    return '\r\n'.join(data)
 
 def get_content_type(filename):
-	import mimetypes
-	return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+    import mimetypes
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
