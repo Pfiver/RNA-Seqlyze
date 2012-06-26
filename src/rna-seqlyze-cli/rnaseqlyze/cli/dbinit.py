1d0adea17c5c53dc56bc21fd29037d36bffe5ad0
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-06-26 23:54:31 +0200

    use rnaseqlyze.group, instead of web_group

--- src/rna-seqlyze-cli/rnaseqlyze/cli/dbinit.py
+++ src/rna-seqlyze-cli/rnaseqlyze/cli/dbinit.py
@@ -7,7 +7,7 @@ The command takes no arguments. The database is expected to be an sqlite
 database and the path to the database file should be set in 'rnaseqlyze.ini'.
 Afte creating the file, it's unix access mode is changed to (octal) 0664, which
 means user/group=read/write,others=read and the group membership is changed to
-<web_group>. <web_group> should be confgured in 'rnaseqlyze.ini' as well.
+<group>. <group> should be confgured in 'rnaseqlyze.ini' as well.
 """
 
 import os, sys, grp
@@ -27,11 +27,11 @@ def main():
     db_path = rnaseqlyze.db_url.split(":", 1)[1]
     # remove the file
     os.unlink(db_path)
-    # recreate it & create all necessary tables
+    # create the database
     Entity.metadata.create_all(create_engine(rnaseqlyze.db_url))
-    # change parmission to user/group=read/write,others=read
+    # change permission bits
     os.chmod(db_path, 0664)
-    # change group ownership to web_server group
-    os.chown(db_path, -1, grp.getgrnam(rnaseqlyze.web_group).gr_gid)
+    # change group membership
+    os.chown(db_path, -1, grp.getgrnam(rnaseqlyze.group).gr_gid)
 
-    print "initialized database:", db_path
+    print "initialized database in", os.sep + db_path.lstrip('/')
