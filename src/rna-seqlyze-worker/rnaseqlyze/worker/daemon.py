371e7f869fad8570413493f12f600a409aaf856b
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-06-27 03:51:56 +0200

    ditch [exe] sections in worker ini files

--- src/rna-seqlyze-worker/rnaseqlyze/worker/daemon.py
+++ src/rna-seqlyze-worker/rnaseqlyze/worker/daemon.py
@@ -5,34 +5,35 @@ Usage:
     rnas-worker (start|stop|restart)
     rnas-worker --development
 
-Start, stop, restart the worker daemon.
+Start, stop or restart the worker daemon.
 """
 
 def main():
     import os
     import docopt
-    import paste.script.exe
+    import paste.script.serve
 
     opts = docopt.docopt(__doc__)
     for command in "start|stop|restart".split('|'):
         if opts[command]:
             mode = "production"
-            args = [command]
+            args = [command, "--daemon"]
             break
     else:
         mode = "development"
-        args = []
+        args = ["--reload"]
 
     if mode == 'production':
         import rnaseqlyze
         args.extend([
             "--user=" + rnaseqlyze.worker_user,
             "--group=" + rnaseqlyze.group,
+            "--log-file=" + rnaseqlyze.worker_log_file,
+            "--pid-file=" + rnaseqlyze.worker_pid_file,
         ])
 
     here = os.path.dirname(os.path.abspath(__file__))
     conf_file = os.path.join(here, '..', '..', mode + '.ini')
 
     args.insert(0, conf_file)
-    os.environ['_'] = conf_file
-    paste.script.exe.ExeCommand("exe").run(args)
+    paste.script.serve.ServeCommand("serve").run(args)
