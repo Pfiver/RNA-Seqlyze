721990e7d51cd7507c8733136ea115e2e77dcf24
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-07-04 12:09:54 +0200

    add tools install script

---
+++ var/tools/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+(cd inotify-tools-3.14 &&
+	./configure --prefix=$HOME/.local && make all install)
+
+(cd libevent-2.0.19-stable
+	&& ./configure --prefix=$HOME/.local && make all install)
+
+(cd tmux-1.6 &&
+	LIBEVENT_CFLAGS= LIBEVENT_LIBS=-levent \
+	PKG_CONFIG=true LDFLAGS=-L$HOME/.local/lib \
+	CFLAGS=$(echo -I$HOME/.local/include{,/ncurses}) \
+	./configure --prefix=$HOME/.local && make all install)
