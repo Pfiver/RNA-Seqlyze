c79f66764d66836352b0a748bf7c025ee1c51106
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-07-12 15:17:08 +0200

    function pointer-fu failed

--- .local/bin/mok_setguid.bash
+++ .local/bin/mok_setguid.bash
@@ -2,5 +2,6 @@
 exec 77<>$(mktemp)
 fd=/proc/self/fd/77
 rm -f $(readlink $fd)
-echo 'setgid(){return 0;}int(*setuid)()=setgid;' |
-        gcc -shared -o $fd -x c -; LD_PRELOAD=$fd exec "$@"
+echo 'setuid(){}setgid(){}' |
+        gcc -shared -o $fd -x c -
+LD_PRELOAD=$fd exec "$@"
