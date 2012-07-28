f44390bba4bbb93c0ec3f8c51346e8df12659dd4
Patrick Pfeifer <patrick.pfeifer@students.fhnw.ch>
2012-07-28 04:56:34 +0200

    perfect install.sh

--- install.sh
+++ install.sh
@@ -15,52 +15,52 @@ TRAC_DB=sqlite:///$WORKDIR-dev/trac.db
 
 # config variables documentation,
 # copied from the 'Deployment' wiki page
-doc_TOPDIR='
+doc_aTOPDIR='
    The root of the project source tree cloned from
    https://mu15ns00002.adm.ds.fhnw.ch/git/biocalc;
    for example `/home/user/rna-seqlyze`.
 '
-doc_PREFIX='
+doc_bPREFIX='
    The project installation directory on the server;
    for example `/home/user/.local` or `/usr/local`, `/opt/biocalc`, etc.
 '
-doc_WORKDIR='
+doc_cWORKDIR='
    A directory on the server where lots of space should be available;
    for example`/home/user/rna-seqlyze-workdir`.
 '
-doc_WWWDIR='
+doc_dWWWDIR='
    The directory on the server containing the
    .htaccess file and the .wsgi scripts;
    for example `/home/user/public_html`.
 '
-doc_WWWBASE='
+doc_eWWWBASE='
    The path under which `WWWDIR` is accessible on the server
    ''from outside'' (e.g. with HTTP on port 80);
    for example `/rna-seqlyze/`.
 '
-doc_BIBODIR='
+doc_fBIBODIR='
    A directory to hold one buildbot "master" and one buildbot
    "slave" base directory;
    for example `/home/user/buildbot`.
 '
 
-doc_HOSTNAME='
+doc_gHOSTNAME='
    The hostname under which the server is accessible from outside;
    for example `www.rna-seqlyze.com`.
 '
-doc_GROUP='
+doc_hGROUP='
    The unix group that the web application and the worker run as;
    for example `www-data`.
 '
-doc_WORKER_USER='
+doc_iWORKER_USER='
    The unix user that the worker runs as;
    for example `www-data`.
 '
-doc_ADMIN_EMAIL='
+doc_jADMIN_EMAIL='
    The email address of the application administrator;
    for example `admin@rna-seqlyze.com`.
 '
-doc_TRAC_DB='
+doc_kTRAC_DB='
    A database url for a database where trac will keep its data;
    for example `sqlite:///$WORKDIR-dev/trac.db`.
 '
@@ -74,7 +74,7 @@ then
     exit 1
 fi
 
-# if not already manually configured at the top of the script,
+# if not manually configured at the top of the script,
 # print the documentation for and ask for the value of each variable
 if [ -z "$ADMIN_EMAIL" ]
 then
@@ -83,7 +83,7 @@ then
     echo "To use the defaults, shown [in brackets], just hit return."
     for doc in ${!doc_*}
     do
-        var=${doc#doc_}
+        var=${doc#doc_?}
         echo
         echo "${!doc}"
         while true
@@ -97,19 +97,74 @@ then
     echo
 fi
 
-# helper function
+# helper functions
 confsub=$(
     for doc in ${!doc_*}
     do
-        var=${doc#doc_}
+        var=${doc#doc_?}
         echo -n "s|@@$var@@|${!var//|/\\|}|g;"
     done
 )
-subcat() { sed "$confsub" "$1"; }
+subcat() {
+    sed "$confsub" "$1"
+}
+su() {
+    if [ "$1" = -v ]
+    then
+        shift
+        echo "$ ${Debian:+command su}${Ubuntu:+sudo sh} -c '$2'"
+    fi
+    ${Debian:+command su}${Ubuntu:+sudo sh} -c "$2"
+}
 
 # remember the current directory
 CURDIR=$PWD
 PATH=$PATH:$PREFIX/bin
+
+# dependencies (su)
+boost=libboost1.49-all-dev
+[ -n "$(apt-cache search $boost)" ] || boost=
+deps=(
+    ntp ssh vim
+    curl bzip2 sqlite3 deborphan python-lxml libapache2-mod-wsgi
+    devscripts gcc-4.3-base
+    cmake libbz2-dev libpng12-dev python-all-dev python3-all-dev $boost
+)
+su -v -c "apt-get install ${deps[*]}"
+
+# boost 1.49 (su)
+if [ -z "$boost" ]
+then
+    tmpdir=$(mktemp -d)
+    base_url=http://ftp.ch.debian.org/debian/pool/main/b/boost1.49
+    curl -JLOOO $base_url/boost1.49_1.49.0{.orig.tar.bz2,-3.1.{dsc,debian.tar.gz}}
+    su -c "apt-get install libicu-dev mpi-default-dev bison flex \
+                           docbook-to-man help2man xsltproc doxygen gccxml"
+    cd $tmpdir
+    dpkg-source -x boost1.49*.dsc
+    cd boost1.49*
+    dpkg-buildpackage -b
+    cd ..
+    su -v -c "dpkg -i *.deb"
+    cd
+    rm -rf $tmpdir
+fi
+
+# add $USER and $WORKER_USER to $GROUP (su)
+gid=$(getent group $GROUP | cut -d : -f 3)
+for user in $USER $WORKER_USER
+do
+    found=false
+    for id in $(id -G)
+    do
+        if [ $id = $gid ]
+        then
+            found=true
+            break
+        fi
+    done
+    $found || su -v -c "usermod -a -G $GROUP $user"
+done
 
 # distribute
 if ! python -c 'import setuptools' 2> /dev/null
@@ -215,6 +270,8 @@ then
     # set up user accounts
     hp=$WWWDIR/.htpasswd.trac
     touch $hp
+    chmod 640 $hp
+    chgrp $GROUP $hp
     echo Add some trac users.
     unset u
     while true
@@ -268,18 +325,16 @@ then
     subcat buildbot-master.cfg > $BIBODIR/buildmaster/master.cfg
 fi
 
-# apache, worker service
+# apache and -worker daemon (su)
 cd $CURDIR
-subcat $TOPDIR/src/rna-seqlyze-worker/rna-seqlyze-service > rna-seqlyze-service
 subcat $TOPDIR/var/conf-files/rna-seqlyze-a2conf > rna-seqlyze-a2conf
-
-# as root
 apache_conf='
 chown root. rna-seqlyze-a2conf
 mv rna-seqlyze-a2conf /etc/apache2/conf.d/rna-seqlyze
 a2enmod proxy rewrite wsgi
 service apache2 restart
 '
+subcat $TOPDIR/src/rna-seqlyze-worker/rna-seqlyze-service > rna-seqlyze-service
 service_conf='
 chown root. rna-seqlyze-service
 chmod 755 rna-seqlyze-service
@@ -291,8 +346,8 @@ cat << END_OF_ROOT
 
 Almost done!
 
-There are two things left to be done
-that need to be carried out with _root permissions_:
+There are two more things left to be done
+that need to be carried out with root permissions:
 
 1. To configure the apache webserver, the file 'rna-seqlyze-a2conf',
    that has just been created in the current directory, must be moved to
@@ -315,7 +370,7 @@ $service_conf
 Enter ${Debian:+the root}${Ubuntu:+your} password to issue those commands now.
 
 END_OF_ROOT
-if ! ${Debian:+su -c}${Ubuntu:+sudo sh -c} "$apache_conf$service_conf"
+if ! su -c "$apache_conf$service_conf"
 then
     echo "$apache_conf$service_conf" > finish-installation
     echo "An error happend trying to issue the mentioned commands."
